31b8b51298bbb30e6fff94f3b525630fd532fc2d
Veeraj <development@gomogroup.com>
2026-07-02 14:52:26 +0530

    contact and supplier modules created

---
+++ src/components/sections/generic/ContactFormSection.jsx
@@ -0,0 +1,7 @@
+"use client";
+
+import SolutionContactFormSection from "../solution/ContactFormSection";
+
+export default function GenericContactFormSection(props) {
+  return <SolutionContactFormSection {...props} />;
+}
