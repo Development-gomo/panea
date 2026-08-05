34c6fc8b47cdbeeaa9c1b18a013db947305265cf
Development <development@gomogroup.com>
2026-08-05 17:40:05 +0530

    changes

--- src/components/NumberedListItem.jsx
+++ src/components/NumberedListItem.jsx
@@ -8,7 +8,7 @@ export default function NumberedListItem({ index, children }) {
       <span className="ff-larken pt-[10px] text-[16px] leading-none font-light text-(--color-body) italic">
         {String(index + 1).padStart(2, "0")}
       </span>
-      <div className="border-b border-[#1E2E31]/20 pb-4">{children}</div>
+      <div className="border-ba border-[#1E2E31]/20 pb-4">{children}</div>
     </article>
   );
 }
